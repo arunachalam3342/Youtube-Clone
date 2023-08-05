68cbfdb281d9cab1c9d8f2457ea535f555b6686f
arunachalam3342 <105549877+arunachalam3342@users.noreply.github.com>
2023-08-05 17:14:04 +0530

    icon change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Box sx={{ backgoundColor: '#000' }}>
         <NavBar />
         <Routes>
-          <Route path='/' exact element={<Feed />} />
+          <Route path='/youtube-clone' exact element={<Feed />} />
           <Route path='/video/:id' element={<VideoDetail />} />
           <Route path='/channel/:id' element={<ChannelDetail />} />
           <Route path='/search/:searchTerm' element={<SearchFeed />} />
